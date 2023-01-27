719fa2e75658daa9757643a057a067fd79b98441
john <ralphesleyer@gmail.com>
2023-01-27 18:18:10 +0800

    added code for user input

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var username, password, name, host, port, tableName string
+
+	fmt.Print("db_username:")
+	fmt.Scan(&username)
+
+	fmt.Print("db_password:")
+	fmt.Scan(&password)
+
+	fmt.Print("db_name:")
+	fmt.Scan(&name)
+
+	fmt.Print("db_host:")
+	fmt.Scan(&host)
+
+	fmt.Print("db_port:")
+	fmt.Scan(&port)
+
+	fmt.Print("db_table_name (table to display rows):")
+	fmt.Scan(&tableName)
+	fmt.Print("-----------------------------------")
 }
