a65c7d75905504d468dcf11150c39febd72b34d1
john <ralphesleyer@gmail.com>
2023-01-27 18:20:48 +0800

    added code for connecting to remote database

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
 	var username, password, name, host, port, tableName string
@@ -23,4 +28,12 @@ func main() {
 	fmt.Print("db_table_name (table to display rows):")
 	fmt.Scan(&tableName)
 	fmt.Print("-----------------------------------")
+
+	// Connect to the RDS database
+	db, err := sql.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+name)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
 }
